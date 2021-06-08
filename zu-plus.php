61eae95a832db9434950abac88fe0671f2dccc28
picasso <dmitry.rudakov@icloud.com>
2021-06-08 18:13:59 +0200

    changed loading order for Zukit

--- zu-plus.php
+++ zu-plus.php
@@ -15,6 +15,11 @@ Requires PHP: 7.4.1
 
 // Prohibit direct script loading
 defined('ABSPATH') || die('No direct script access allowed!');
+
+// Always load Zukit even if we don't use it later ('wp_doing_ajax' or 'wp_doing_cron')
+// as other plugins or themes may want to use it
+require_once('zukit/load.php');
+
 // Exit early if a WordPress heartbeat comes
 if(wp_doing_ajax() && isset($_POST['action']) && ($_POST['action'] === 'heartbeat')) return;
 // Let's not load plugin during cron events
@@ -23,8 +28,6 @@ if(wp_doing_cron()) return;
 // Start! ---------------------------------------------------------------------]
 
 // add_action('plugins_loaded', function() { 	// DEBUG ONLY
-
-require_once('zukit/load.php');
 
 // compatibility check for Zukit
 if(Zukit::is_compatible(__FILE__, array('min_php' => '7.4.1') )) {
