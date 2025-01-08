ee6d3e92cf5efc62521586d2cc95f795b1fb6bd9
picasso <dmitry.rudakov@me.com>
2025-01-08 17:48:40 +0100

    refactor additional scripts

--- scripts/zuplus-remove-backups.js
+++ scripts/zuplus-remove-backups.js
@@ -1,40 +1,43 @@
-// WordPress dependencies
+/* eslint-disable no-use-before-define */
+import { get, once, set } from 'lodash-es'
 
-const { get, set, once } = lodash;
-const { subscribe, select } = wp.data;
-const { getCurrentPostId, isEditedPostNew } = select('core/editor');
+// WordPress dependencies
+import { select, subscribe } from '@wordpress/data'
+const { getCurrentPostId, isEditedPostNew } = select('core/editor')
 
 function resetPostEditorAutosave(origin = 'ZUKIT') {
-	const resetKey = 'resetAutosave';
+	const resetKey = 'resetAutosave'
 	// may have already been initiated in another plugin or theme
-	if(!get(wp, ['zukit', resetKey])) {
-		set(wp, ['zukit', resetKey], true);
+	if (!get(wp, ['zukit', resetKey])) {
+		set(wp, ['zukit', resetKey], true)
 		const unsubscribe = subscribe(() => {
-			const postId = getCurrentPostId();
-			if(postId) {
-				resetLocalBackup(postId);
-				unsubscribe();
+			const postId = getCurrentPostId()
+			if (postId) {
+				resetLocalBackup(postId)
+				unsubscribe()
 			}
-		});
+		})
 
 		const resetLocalBackup = (postId) => {
-			if(hasSessionStorageSupport()) {
-				const isPostNew = isEditedPostNew();
+			if (hasSessionStorageSupport()) {
+				const isPostNew = isEditedPostNew()
 
-				let localAutosave = localAutosaveGet(postId, isPostNew);
-				if(!localAutosave) return;
+				let localAutosave = localAutosaveGet(postId, isPostNew)
+				if (!localAutosave) return
 
 				try {
-					localAutosave = JSON.parse(localAutosave);
-				} catch (error) {
+					localAutosave = JSON.parse(localAutosave)
+				} catch (_error) {
 					// not usable if it can't be parsed.
-					return;
+					return
 				}
 				// if 'localAutosave' was found, it can be safely ejected from storage.
-				localAutosaveClear(postId, isPostNew);
-				window.console.warn(`${origin.toUpperCase()}: The backup of the post [${postId}] in your browser was removed`);
+				localAutosaveClear(postId, isPostNew)
+				window.console.warn(
+					`${origin.toUpperCase()}: The backup of the post [${postId}] in your browser was removed`,
+				)
 			}
-		};
+		}
 	}
 }
 
@@ -45,27 +48,27 @@ const hasSessionStorageSupport = once(() => {
 		// Private Browsing in Safari 10 and earlier will throw an error when
 		// attempting to set into sessionStorage. The test here is intentional in
 		// causing a thrown error as condition bailing from local autosave.
-		window.sessionStorage.setItem('__wpEditorTestSessionStorage', '');
-		window.sessionStorage.removeItem('__wpEditorTestSessionStorage');
-		return true;
-	} catch ( error ) {
-		return false;
+		window.sessionStorage.setItem('__wpEditorTestSessionStorage', '')
+		window.sessionStorage.removeItem('__wpEditorTestSessionStorage')
+		return true
+	} catch (_error) {
+		return false
 	}
-});
+})
 
 // Function returning a sessionStorage key to set or retrieve a given post's
 // automatic session backup.
 
 function postKey(postId, isPostNew) {
-	return `wp-autosave-block-editor-post-${isPostNew ? 'auto-draft' : postId}`;
+	return `wp-autosave-block-editor-post-${isPostNew ? 'auto-draft' : postId}`
 }
 
 function localAutosaveGet(postId, isPostNew) {
-	return window.sessionStorage.getItem(postKey(postId, isPostNew));
+	return window.sessionStorage.getItem(postKey(postId, isPostNew))
 }
 
 function localAutosaveClear(postId, isPostNew) {
-	window.sessionStorage.removeItem(postKey(postId, isPostNew));
+	window.sessionStorage.removeItem(postKey(postId, isPostNew))
 }
 
-resetPostEditorAutosave('Zu Plus');
+resetPostEditorAutosave('Zu Plus')
